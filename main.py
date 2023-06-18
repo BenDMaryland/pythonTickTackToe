1de0d457da47f8839a2a950a684d0804fe32c852
Benjamin J Darago <ben.d.maryland@gmail.com>
2023-06-18 01:26:51 -0400

    basic setup finished

--- main.py
+++ main.py
@@ -3,11 +3,22 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-board_state = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9']]
+board_state = [['0A', '1B', '2C'], ['3D', '4E', '5F'], ['6G', '7H', '8I']]
+
 
 print(*board_state, sep="\n")
 
-print('What would you like to pick:')
-player_x_pick = input()
+for i in range(9):
+    print('What would you like to pick:')
+    number_as_string = input()
+    number_as_int = int(number_as_string)
 
-print(board_state[1][1])
+    y=0
+    remainder = number_as_int % 3
+
+    if number_as_int > 2:
+        y=1
+        if number_as_int > 5:
+            y=2
+
+    print(board_state[y][remainder])
